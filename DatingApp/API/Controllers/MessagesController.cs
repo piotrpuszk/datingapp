17274f65966d406a154dfe214f6d6ffc9aa91efa
puszkars <piotrpuszk@gmail.com>
2021-04-16 17:26:59 +0200

    End of section 18

--- DatingApp/API/Controllers/MessagesController.cs
+++ DatingApp/API/Controllers/MessagesController.cs
@@ -14,17 +14,14 @@ namespace API.Controllers
     [Authorize]
     public class MessagesController : BaseApiController
     {
-        private readonly IUserRepository _userRepository;
-        private readonly IMessageRepository _messagesRepository;
         private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public MessagesController(IUserRepository userRepository,
-        IMessageRepository messagesRepository,
+        public MessagesController(IUnitOfWork unitOfWork,
         IMapper mapper)
         {
+            _unitOfWork = unitOfWork;
             _mapper = mapper;
-            _messagesRepository = messagesRepository;
-            _userRepository = userRepository;
         }
 
         [HttpPost]
@@ -35,8 +32,8 @@ namespace API.Controllers
             {
                 return BadRequest("You cannot send messages to yourself");
             }
-            var sender = await _userRepository.GetUserByUsernameAsync(username);
-            var recipient = await _userRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
+            var sender = await _unitOfWork.UserRepository.GetUserByUsernameAsync(username);
+            var recipient = await _unitOfWork.UserRepository.GetUserByUsernameAsync(createMessageDto.RecipientUsername);
             if (recipient == null) return NotFound();
             var message = new Message
             {
@@ -46,39 +43,32 @@ namespace API.Controllers
                 RecipientUsername = recipient.UserName,
                 Content = createMessageDto.Content,
             };
-            _messagesRepository.AddMessage(message);
-            if(await _messagesRepository.SaveAllAsync()) return Ok(_mapper.Map<MessageDto>(message));
+            _unitOfWork.MessageRepository.AddMessage(message);
+            if (await _unitOfWork.Complete()) return Ok(_mapper.Map<MessageDto>(message));
             return BadRequest("Failed to send message");
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser([FromQuery] 
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser([FromQuery]
         MessageParams messageParams)
         {
             messageParams.Username = User.GetUsername();
-            var messages = await _messagesRepository.GetMessagesForUser(messageParams);
-            Response.AddPaginationHeader(messages.CurrentPage, messages.PageSize, 
+            var messages = await _unitOfWork.MessageRepository.GetMessagesForUser(messageParams);
+            Response.AddPaginationHeader(messages.CurrentPage, messages.PageSize,
             messages.TotalCount, messages.TotalPages);
             return messages;
-        }
-
-        [HttpGet("thread/{username}")]
-        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
-        {
-            var currentUsername = User.GetUsername();
-            return Ok(await _messagesRepository.GetMessageThread(currentUsername, username));
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteMessage(int id)
         {
             var username = User.GetUsername();
-            var message = await _messagesRepository.GetMessage(id);
-            if(message.Sender.UserName != username && message.Recipient.UserName != username) return Unauthorized();
-            if(message.Sender.UserName == username) message.SenderDeleted = true;
-            if(message.Recipient.UserName == username) message.RecipientDeleted = true;
-            if(message.SenderDeleted && message.RecipientDeleted) _messagesRepository.DeleteMessage(message);
-            if(await _messagesRepository.SaveAllAsync()) return Ok();
+            var message = await _unitOfWork.MessageRepository.GetMessage(id);
+            if (message.Sender.UserName != username && message.Recipient.UserName != username) return Unauthorized();
+            if (message.Sender.UserName == username) message.SenderDeleted = true;
+            if (message.Recipient.UserName == username) message.RecipientDeleted = true;
+            if (message.SenderDeleted && message.RecipientDeleted) _unitOfWork.MessageRepository.DeleteMessage(message);
+            if (await _unitOfWork.Complete()) return Ok();
             return BadRequest("Problem deleting the message");
         }
     }
--- DatingApp/API/Data/MessageRepository.cs
+++ DatingApp/API/Data/MessageRepository.cs
@@ -74,35 +74,34 @@ namespace API.Data
         {
             var query = _context.Messages
             .OrderByDescending(m => m.MessageSent)
+            .ProjectTo<MessageDto>(_mapper.ConfigurationProvider)
             .AsQueryable();
 
             query = messageParams.Container switch
             {
-                "Inbox" => query.Where(u => u.Recipient.UserName == messageParams.Username && !u.RecipientDeleted),
-                "Outbox" => query.Where(u => u.Sender.UserName == messageParams.Username && !u.SenderDeleted),
+                "Inbox" => query.Where(u => u.RecipientUsername == messageParams.Username && !u.RecipientDeleted),
+                "Outbox" => query.Where(u => u.SenderUsername == messageParams.Username && !u.SenderDeleted),
                 _ => query.Where(u =>
-                u.Recipient.UserName == messageParams.Username && !u.RecipientDeleted && u.DateRead == null)
+                u.RecipientUsername == messageParams.Username && !u.RecipientDeleted && u.DateRead == null)
             };
-            var messages = query.ProjectTo<MessageDto>(_mapper.ConfigurationProvider);
             return await PagedList<MessageDto>
-            .CreateAsync(messages, messageParams.PageNumber, messageParams.PageSize);
+            .CreateAsync(query, messageParams.PageNumber, messageParams.PageSize);
         }
 
         public async Task<IEnumerable<MessageDto>> GetMessageThread(string currentUsername,
         string recipientUsername)
         {
             var messages = await _context.Messages
-            .Include(u => u.Sender).ThenInclude(p => p.Photos)
-            .Include(u => u.Recipient).ThenInclude(p => p.Photos)
             .Where(m => m.Recipient.UserName == currentUsername && !m.RecipientDeleted
                 && m.Sender.UserName == recipientUsername
                 || m.Recipient.UserName == recipientUsername
                 && m.Sender.UserName == currentUsername && !m.SenderDeleted
             )
             .OrderBy(m => m.MessageSent)
+            .ProjectTo<MessageDto>(_mapper.ConfigurationProvider)
             .ToListAsync();
             var unreadMessages = messages.Where(m =>
-            m.DateRead == null && m.Recipient.UserName == currentUsername).ToList();
+            m.DateRead == null && m.RecipientUsername == currentUsername).ToList();
 
             if (unreadMessages.Any())
             {
@@ -110,14 +109,8 @@ namespace API.Data
                 {
                     message.DateRead = DateTime.UtcNow;
                 }
-                await _context.SaveChangesAsync();
             }
-            return _mapper.Map<IEnumerable<MessageDto>>(messages);
-        }
-
-        public async Task<bool> SaveAllAsync()
-        {
-            return await _context.SaveChangesAsync() > 0;
+            return messages;
         }
     }
 }
